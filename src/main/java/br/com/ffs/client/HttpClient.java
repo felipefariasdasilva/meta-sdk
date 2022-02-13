902a349604163587507b0b837b22baa7fedc78de
felipefariasdasilva <feliipefarias@outlook.com>
2022-02-13 08:20:31 -0300

    refactor: bring httpClient to agnostic context;

--- src/main/java/br/com/ffs/client/HttpClient.java
+++ src/main/java/br/com/ffs/client/HttpClient.java
@@ -1,8 +1,31 @@
 package br.com.ffs.client;
 
+import lombok.AllArgsConstructor;
+import lombok.val;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Component
+@AllArgsConstructor
 public class HttpClient {
+
+    private final RestTemplate restTemplate;
+
+    public <REQUEST, RESPONSE> RESPONSE doGet(
+        final Class<RESPONSE> responseClass){
+
+        val headers = getHeaders();
+        val entity = new HttpEntity<>(headers);
+        return restTemplate.exchange(HttpMethod.GET, entity, responseClass);
+    }
+
+    public void doPost(){}
+
+    private HttpHeaders getHeaders() {
+        return new HttpHeaders();
+    }
 
 }
